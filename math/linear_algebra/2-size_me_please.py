a0b725a51b75419433b3874e49de4d6b376edc3c
FrancoMuVa <muvafranco@gmail.com>
2024-04-09 18:01:50 +0000

    fix: fixed function

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    row, col = len(matrix), len(matrix[0])
-    if isinstance(matrix[0][0], list):
-        sli = len(matrix[0][0])
-        return [row, col, sli]
-    return [row, col]
+    row = len(matrix)
+    if isinstance(matrix[0], list):
+        col = len(matrix[0])
+        if isinstance(matrix[0][0], list):
+            sli = len(matrix[0][0])
+            return [row, col, sli]
+        return [row, col]
+    return [row]
