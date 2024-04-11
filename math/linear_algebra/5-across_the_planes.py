5867c99bd8ed134ce40e300e82661cba9332765d
FrancoMuVa <muvafranco@gmail.com>
2024-04-11 04:41:09 +0000

    feat: Function that adds two matrices element-wise

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Function that adds two """
+
+
+def add_matrices2D(mat1, mat2):
+    """ Returns the  """
+    length = len(mat1[0]) == len(mat2[0])
+    if length:
+        matrix = []
+        for row_1, row_2 in zip(mat1, mat2):
+            row = []
+            for i in range(0, len(row_1)):
+                row.append(row_1[i] + row_2[i])
+            matrix.append(row)
+        return matrix
+    elif length:
+        return None
