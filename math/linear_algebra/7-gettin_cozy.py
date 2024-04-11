e336f61c1ecbfcfde4fbb8108ebbc262e7b121aa
FrancoMuVa <muvafranco@gmail.com>
2024-04-11 22:16:16 +0000

    fix: dimensions don’t match error

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,14 +6,16 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """ Return a new matrix """
-    if len(mat1[0]) != len(mat2[0]):
-        return None
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         matrix = [row[:] for row in mat1]
         for row2 in mat2:
             matrix.append(row2[:])
         return matrix
     elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         matrix = []
         for row1, row2 in zip(mat1, mat2):
             matrix.append(row1[:] + row2[:])
