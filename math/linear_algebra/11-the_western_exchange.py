b803f4c0310db02fcc26ecc8af14a35940630015
FrancoMuVa <muvafranco@gmail.com>
2024-04-13 23:37:44 +0000

    feat: Function that transposes matrix.

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Function that transposes matrix.
+"""
+
+
+def np_transpose(matrix):
+    """ Return a new numpy.ndarray """
+    new_mat = np.transpose(matrix)
+    return new_mat
