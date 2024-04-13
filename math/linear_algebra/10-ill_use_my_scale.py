3ea73a3ab019a25fcba48728ed7e0bc83635e76b
FrancoMuVa <muvafranco@gmail.com>
2024-04-13 21:16:08 +0000

    feat: Function that calculates the shape of a numpy.ndarray.

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Function that calculates the shape of a numpy.ndarray.
+"""
+import numpy
+
+
+def np_shape(matrix):
+    """ Return a tuple """
+    return matrix.shape
