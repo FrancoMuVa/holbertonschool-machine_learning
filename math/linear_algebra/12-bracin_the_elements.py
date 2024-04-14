b9d3420d0d5fcc7c673d9bea9cc67a22f9632a6b
FrancoMuVa <muvafranco@gmail.com>
2024-04-14 01:01:44 +0000

    feat: Function that returns matrix operations

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Function  that performs element-wise addition, subtraction,
+    multiplication, and division.
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """ Returns matrix operations """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return add, sub, mul, div
+    
