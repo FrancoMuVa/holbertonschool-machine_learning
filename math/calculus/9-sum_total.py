bab9fd6ffa0feb22343db2965a4db64d8e76a072
FrancoMuVa <muvafranco@gmail.com>
2024-04-21 19:16:23 +0000

    fix: task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,6 +6,7 @@
 
 def summation_i_squared(n):
     """ Return a integer """
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 1:
         return None
-    return ((n * ((n + 1) * (2 * n + 1))) // (6))
+
+    return (n * (n + 1) * (2 * n + 1)) // 6
