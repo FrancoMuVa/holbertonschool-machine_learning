976deb064b37ebacbfa113fbae2f0ac39a94a324
FrancoMuVa <muvafranco@gmail.com>
2024-05-12 22:26:58 +0000

    feat: task 23

---
+++ supervised_learning/classification/23-deep_neural_network.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+"""
+    Class DeepNeuralNetwork
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class DeepNeuralNetwork():
+    """ DeepNeuralNetwork class """
+
+    def __init__(self, nx, layers):
+        """ Initializes a new instance of DeepNeuralNetwork """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(1, len(layers) + 1):
+            if not isinstance(layers[i - 1], int) and layers[i - 1] <= 0:
+                raise TypeError('layers must be a list of positive integers')
+
+            if i == 1:
+                self.__weights['W' + str(i)] = np.random.randn(
+                    layers[i - 1],
+                    nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights['W' + str(i)] = np.random.randn(
+                    layers[i - 1],
+                    layers[i - 2]) * np.sqrt(2 / layers[i - 2])
+            self.__weights['b' + str(i)] = np.zeros((layers[i - 1], 1))
+
+    @property
+    def L(self):
+        """ Getter method """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Getter method """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Getter method """
+        return self.__weights
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation of the neural network """
+        self.__cache['A0'] = X
+        for i in range(1, self.__L + 1):
+            x = np.dot(self.__weights['W' + str(i)],
+                       self.__cache['A' + str(i - 1)])
+            + self.__weights['b' + str(i)]
+            A = 1 / (1 + np.exp(-x))
+            self.__cache['A' + str(i)] = A
+        return A, self.__cache
+
+    def cost(self, Y, A):
+        """ Calculates the cost of the model using logistic regression """
+        m = Y.shape[1]
+        return -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neural network's predictions """
+        self.forward_prop(X)
+        A = self.__cache['A' + str(self.__L)]
+        pred = (A >= 0.5).astype(int)
+        return pred, self.cost(Y, A)
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """ Calculates one pass of gradient descent on the neural network """
+        m = Y.shape[1]
+        dz = cache['A' + str(self.__L)] - Y
+        for i in range(self.__L, 0, -1):
+            A = self.__cache['A' + str(i - 1)]
+            dw = (1 / m) * np.dot(dz, np.transpose(A))
+            db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+            dz = np.dot(np.transpose(self.__weights['W' + str(i)]), dz)\
+                * A * (1 - A)
+            self.__weights['W' + str(i)] -= alpha * dw
+            self.__weights['b' + str(i)] -= alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True,
+              graph=True, step=100):
+        """ Trains the deep neural network """
+        if not isinstance(iterations, int):
+            raise TypeError('iterations must be an integer')
+        elif iterations < 0:
+            raise ValueError('iterations must be a positive integer')
+        if not isinstance(alpha, float):
+            raise TypeError('alpha must be a float')
+        elif alpha < 0:
+            raise ValueError('alpha must be positive')
+        if not isinstance(step, int):
+            raise TypeError('step must be an integer')
+        elif step < 0 or step > iterations:
+            raise ValueError('step must be positive and <= iterations')
+        x_iter, y_cost = [], []
+        for it in range(iterations + 1):
+            A, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+            cost = self.cost(Y, A)
+            if (it % step == 0):
+                if verbose:
+                    print(f'Cost after {it} iterations: {cost}')
+                if graph:
+                    x_iter.append(it)
+                    y_cost.append(cost)
+        plt.plot(x_iter, y_cost, '-', c='blue')
+        plt.title('Training Cost')
+        plt.xlabel('iteration')
+        plt.ylabel('cost')
+        plt.show()
+        return self.evaluate(X, Y)
