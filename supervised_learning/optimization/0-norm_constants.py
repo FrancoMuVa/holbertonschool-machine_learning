4f8f6e212179a5bef45d78bbc49f0fbfbdcdca57
FrancoMuVa <muvafranco@gmail.com>
2024-05-18 18:46:53 +0000

    feat: function that calculates the normalization (standardization) constants of a matrix

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Normalization constants
+"""
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+        Calculates the normalization (standardization) constants of a matrix
+    """
+    m = np.mean(X, axis=0)
+    s = np.std(X, axis=0)
+    return m, s
