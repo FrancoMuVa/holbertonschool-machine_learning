8a070f2fe9d0b87068de28b68644dc0b3dd45a7a
FrancoMuVa <muvafranco@gmail.com>
2024-05-20 01:53:40 +0000

    feat: function that creates a batch normalization layer for a neural network in tensorflow.

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    Optimization
+"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """
+        Creates a batch normalization layer for a
+        neural network in tensorflow.
+    """
+    epsilon = 1e-7
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    x = tf.keras.layers.Dense(n, activation=activation,
+                              kernel_initializer=init, name='layer')(prev)
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+    mean, variance = tf.nn.moments(x, axes=0)
+    return activation(tf.nn.batch_normalization(x, mean, variance,
+                                                beta, gamma, epsilon))
