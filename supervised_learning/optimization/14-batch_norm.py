f743c7186d37bf7bc163d5075e9783852323321f
FrancoMuVa <muvafranco@gmail.com>
2024-05-20 02:02:18 +0000

    fix: activation

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -12,10 +12,10 @@ def create_batch_norm_layer(prev, n, activation):
     """
     epsilon = 1e-7
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    x = tf.keras.layers.Dense(n, activation=activation,
-                              kernel_initializer=init, name='layer')(prev)
+    x = tf.keras.layers.Dense(n, kernel_initializer=init, name='layer')(prev)
     gamma = tf.Variable(tf.ones([n]), trainable=True)
     beta = tf.Variable(tf.zeros([n]), trainable=True)
-    mean, variance = tf.nn.moments(x, axes=0)
-    return activation(tf.nn.batch_normalization(x, mean, variance,
-                                                beta, gamma, epsilon))
+    mean, variance = tf.nn.moments(x, axes=[0])
+    act = tf.nn.batch_normalization(x, mean, variance,
+                                    beta, gamma, epsilon)
+    return activation(act)
