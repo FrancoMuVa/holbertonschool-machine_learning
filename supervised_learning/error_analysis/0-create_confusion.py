fdda8d78f4b53ac0cddcd48e344160d6b6dcf86e
FrancoMuVa <muvafranco@gmail.com>
2024-05-25 21:21:16 +0000

    style: pycodestyle

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -12,7 +12,7 @@ def create_confusion_matrix(labels, logits):
     conf_matrix = np.zeros((classes, classes), dtype=float)
     true_classes = np.argmax(labels, axis=1)
     pred_classes = np.argmax(logits, axis=1)
-    
+
     for true, pred in zip(true_classes, pred_classes):
         conf_matrix[true, pred] += 1
     return conf_matrix
