6455db8b1e80e818f2d05024fef553fe36e2609e
FrancoMuVa <muvafranco@gmail.com>
2024-05-25 21:14:57 +0000

    feat: function that creates a confution matrix

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Error analysis
+"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """ Creates a confusion matrix """
+    classes = labels.shape[1]
+    conf_matrix = np.zeros((classes, classes), dtype=int)
+    true_classes = np.argmax(labels, axis=1)
+    pred_classes = np.argmax(logits, axis=1)
+    for true, pred in zip(true_classes, pred_classes):
+        conf_matrix[true, pred] += 1
+    return conf_matrix
