dbcb71a63c8c6f7a9754014420ed63919216ddb9
FrancoMuVa <muvafranco@gmail.com>
2024-05-28 23:38:50 +0000

    feat: function that builds a neural network with the Keras library

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Tensorflow 2 & Keras
+"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ Builds a neural network with the Keras library """
+    input = K.Input(shape=(nx,))
+
+    model_x = K.layers.Dense(
+        layers[0],
+        activation=activations[0],
+        kernel_regularizer=K.regularizers.L2(lambtha))(input)
+    model_x = K.layers.Dropout(1 - keep_prob)(model_x)
+
+    for i in range(1, len(layers)):
+        model_x = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.L2(lambtha))(model_x)
+        if i < len(layers) - 1:
+            model_x = K.layers.Dropout(1 - keep_prob)(model_x)
+
+    model = K.Model(inputs=input, outputs=model_x)
+    return model
