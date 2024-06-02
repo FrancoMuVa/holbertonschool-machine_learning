19e0e6bcdd761afcb944636e6ce0a5ab015170ea
FrancoMuVa <muvafranco@gmail.com>
2024-06-02 12:53:07 +0000

    feat: Function that saves an entire model and function that loads an entire model

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Tensorflow 2 & Keras
+"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """ Function that saves an entire model """
+    network.save(filpath=filename)
+    return None
+
+
+def load_model(filename):
+    """ Function that loads an entire model """
+    model = K.saving.load_model(filepath=filename)
+    return model
