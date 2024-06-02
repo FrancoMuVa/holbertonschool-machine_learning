f0dc034141d07eb49478557b3ff3cedc66cc7e55
FrancoMuVa <muvafranco@gmail.com>
2024-06-02 07:58:34 +0000

    feat: Trains a model using mini-batch gradient descent

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -8,5 +8,6 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs, verbose=True,
                 shuffle=False):
     """ Trains a model using mini-batch gradient descent """
-    return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
-                       verbose=verbose, shuffle=shuffle)
+    History = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle)
+    return History
