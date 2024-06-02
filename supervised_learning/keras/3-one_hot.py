07d47236049185d8e368e204214c8a7155d9b8ff
FrancoMuVa <muvafranco@gmail.com>
2024-06-02 06:31:43 +0000

    feat: Converts a label vector into a one-hot matrix

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -2,13 +2,9 @@
 """
 Tensorflow 2 & Keras
 """
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """ Converts a label vector into a one-hot matrix """
-    if classes is None:
-        classes = np.max(labels) + 1
-    matrix = np.zeros((len(labels), classes))
-    matrix[np.arange(len(labels)), labels] = 1
-    return matrix
+    return K.utils.to_categorical(labels, num_classes=classes)
