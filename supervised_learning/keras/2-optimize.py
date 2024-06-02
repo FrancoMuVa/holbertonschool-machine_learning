a31d0d83369ba0066ff179f7e871d66aab85a3db
FrancoMuVa <muvafranco@gmail.com>
2024-06-02 08:11:33 +0000

    fix: task

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -14,4 +14,5 @@ def optimize_model(network, alpha, beta1, beta2):
         learning_rate=alpha,
         beta_1=beta1,
         beta_2=beta2)
-    network.compile(optimizer=optimizer, loss='categorical_crossentropy')
+    network.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                    metrics=["accuracy"])
--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -8,6 +8,5 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs, verbose=True,
                 shuffle=False):
     """ Trains a model using mini-batch gradient descent """
-    History = network.fit(x=data, y=labels, batch_size=batch_size,
-                          epochs=epochs, verbose=verbose, shuffle=shuffle)
-    return History
+    return network.fit(x=data, y=labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose, shuffle=shuffle)
