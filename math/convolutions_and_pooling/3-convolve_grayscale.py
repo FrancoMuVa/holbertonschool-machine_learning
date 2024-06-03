4089ae54f1bfb24bffa5d2b19cf4ca787b5bfe26
FrancoMuVa <muvafranco@gmail.com>
2024-06-03 00:15:01 +0000

    feat: Function that performs a convolution on grayscale images

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Convolutions and Pooling
+"""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """ Function that performs a convolution on grayscale images """
+    m, he, wi = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == "same":
+        ph = ((he - 1) * sh + kh) // 2 + 1
+        pw = ((wi - 1) * sw + kw) // 2 + 1
+    elif padding == "valid":
+        ph = 0
+        pw = 0
+    else:
+        ph, pw = padding
+
+    pad_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
+    h_out = (he + ph * 2 - kh) // sh + 1
+    w_out = (wi + pw * 2 - kw) // sw + 1
+    comv = np.zeros((m, h_out, w_out))
+    img = np.arange(m)
+
+    for h in range(h_out):
+        for w in range(w_out):
+            sect = pad_img[:, h * sh:((h * sh) + kh), w * sw:((w * sw) + kw)]
+            comv[img, h, w] = np.sum(sect * kernel, axis=(1, 2))
+    return comv
