97a05fb5c3e6e93acb0482dd964e0c9c6d1af094
FrancoMuVa <muvafranco@gmail.com>
2024-06-09 17:42:02 +0000

    fix: task

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -23,14 +23,13 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     pad_img = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
     h_out = (h_prev + ph * 2 - kh) // sh + 1
     w_out = (w_prev + pw * 2 - kw) // sw + 1
-
     conv = np.zeros((m, h_out, w_out, c_new))
-    c = np.arange(c_new)
 
     for h in range(h_out):
         for w in range(w_out):
             sect = pad_img[:, h * sh:((h * sh) + kh),
                            w * sw:((w * sw) + kw), :]
-            conv[:, h, w, c] = np.tensordot(sect, W[:, :, :, c],
-                                            axes=([1, 2, 3], [0, 1, 2])) + b
+            for c in range(c_new):
+                conv[:, h, w, c] = np.sum(sect * W[:, :, :, c], axis=(1, 2, 3))
+    conv += b
     return activation(conv)
