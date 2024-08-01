eb914f823dba4dbdf195d8f9d3b05a58a729075e
FrancoMuVa <muvafranco@gmail.com>
2024-08-01 23:42:46 +0000

    feat: task 9 - 14

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Function that calculates the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    " Return a shape of a numpy.ndarray"
+    return matrix.shape
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Function that transposes matrix
+"""
+
+
+def np_transpose(matrix):
+    " Returns a new numpy.ndarray "
+    return matrix.transpose()
