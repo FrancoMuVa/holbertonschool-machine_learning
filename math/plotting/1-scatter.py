b59179717437b1c930ab1c25a747fcd179fea2fd
FrancoMuVa <muvafranco@gmail.com>
2024-08-08 15:52:13 -0300

    fix: magenta points

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -15,8 +15,8 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(x, y, 'm.')
-    plt.title("Men's Height vs Weight")
+    plt.plot(x, y, 'mo')
+    plt.title('Men\'s Height vs Weight')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.show()
