884e45830857f65c94cb5bec7bbcea22a39b74c8
FrancoMuVa <muvafranco@gmail.com>
2024-08-11 18:52:56 -0300

    fix: fixed answer and function

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,4 +6,5 @@
 
 def summation_i_squared(n):
     " Retutn the sum of squeres from 1 to n "
-    return sum(i ** 2 for i in range(1, n + 1))
+    return (n * (n + 1) * (2 * n + 1)) // 6 if isinstance(n, int) and n >= 1\
+        else None
