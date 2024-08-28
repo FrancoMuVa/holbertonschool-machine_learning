220f973ffa681d1dcc6564df356335c3b544b87a
FrancoMuVa <muvafranco@gmail.com>
2024-08-28 08:57:33 -0300

    feat: new class DeepNeuralNetwork

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Class that define a Deep Neural Network
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class DeepNeuralNetwork """
+    def __init__(self, nx, layers):
+        " Initializes a new instance of DeepNeuralNetwork "
+        if not isinstance(nx, int):
+            raise TypeError('x must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for i in range(1, len(layers) + 1):
+            if not isinstance(layers[i - 1], int):
+                raise TypeError('layers must be a list of positive integers')
+            if i == 1:
+                self.weights['W' + str(i)] = np.random.randn(
+                    layers[i - 1], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights['W' + str(i)] = np.random.randn(
+                    layers[i - 1], layers[i - 2]) * np.sqrt(2 / layers[i - 2])
+            self.weights['b' + str(i)] = np.zeros((layers[i - 1], 1))
