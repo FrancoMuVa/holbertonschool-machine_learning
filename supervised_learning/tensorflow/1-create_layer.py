fe5befaf43ce1be440c8a796bf3f80ab3450002e
FrancoMuVa <muvafranco@gmail.com>
2024-09-04 09:11:18 -0300

    fix: fixed initialization

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -8,6 +8,6 @@ import tensorflow.compat.v1 as tf
 def create_layer(prev, n, activation):
     " Creates a layer "
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    ly = tf.keras.layers.Dense(units=n, name='layer', activation=activation,
-                               bias_initializer=init)
-    return ly(prev)
+    layer = tf.keras.layers.Dense(units=n, name='layer', activation=activation,
+                               kernel_initializer=init)
+    return layer(prev)
