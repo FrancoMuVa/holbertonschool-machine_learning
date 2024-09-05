469fee00f25769b21d98d6e2ddd263e835fc07f0
FrancoMuVa <muvafranco@gmail.com>
2024-09-05 16:44:32 -0300

    fix: compared the predicted classes with the actual input data

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -7,6 +7,8 @@ import tensorflow.compat.v1 as tf
 
 def calculate_accuracy(y, y_pred):
     " Calculates the accuracy of a prediction "
-    bool_tensor = tf.math.equal(y, y_pred)
+    idx_data = tf.argmax(y, 1)
+    idx_pred_classes = tf.argmax(y_pred, 1)
+    bool_tensor = tf.math.equal(idx_data, idx_pred_classes)
     accuracy = tf.reduce_mean(tf.cast(bool_tensor, dtype=tf.float32))
     return accuracy
