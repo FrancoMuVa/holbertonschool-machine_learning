566a312f8858c6a5efeea88bc720b351ce82bd7f
FrancoMuVa <muvafranco@gmail.com>
2024-09-08 18:39:54 -0300

    feat: function that sets up Adam optimization for a keras model with categorical crossentropy loss and accuracy metrics

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Optimize
+"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Sets up Adam optimization for a keras model with
+    categorical crossentropy loss and accuracy metrics
+    """
+    op = K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+    )
+    network.compile(
+        optimizer=op,
+        loss=K.losses.CategoricalCrossentropy(),
+        metrics=[K.metrics.Accuracy()]
+    )
+    return None
