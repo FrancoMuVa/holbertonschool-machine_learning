ef37cdcb8d2b95fb1690bc68ad549d1bae9fbf21
FrancoMuVa <muvafranco@gmail.com>
2024-09-08 21:47:32 -0300

    update: based on task 6, train the model with learning rate decay

---
+++ supervised_learning/keras/7-train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Learning Rate Decay
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent,
+    analyze validation data and train the model with learning rate decay.
+    """
+    callback = []
+    if learning_rate_decay and validation_data:
+        callback.append(K.callbacks.LearningRateScheduler(
+            schedule=(lambda epochs:
+                    alpha / 1 + decay_rate * epochs),
+            verbose=True))
+    if early_stopping:
+        callback.append(K.callbacks.EarlyStopping('val_loss',
+                                                  patience=patience))
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        callbacks=callback,
+        validation_data=validation_data,
+        shuffle=shuffle)
