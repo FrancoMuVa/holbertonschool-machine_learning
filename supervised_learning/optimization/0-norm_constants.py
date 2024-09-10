eab67b0da4ee1692ab0cfbf314edb76ad3663b37
FrancoMuVa <muvafranco@gmail.com>
2024-09-10 15:20:05 +0000

    doc: added comment documentation

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+"""
+    Normalization Constants
+"""
 import numpy as np
 
 
 def normalization_constants(X):
-    " calculates the normalization (standardization) constants of a matrix "
+    " Returns: the mean and standard deviation of each feature, respectively "
     return np.mean(X, axis=0), np.std(X, axis=0)
