1f022b94885c995b1d0c75a78bf83033195060a1
FrancoMuVa <muvafranco@gmail.com>
2024-09-30 20:25:43 -0300

    fix: shape of convolution

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,10 +9,10 @@ def convolve_grayscale_valid(images, kernel):
     " Performs a valid convolution on grayscale images "
     m, h, w, = images.shape
     kh, kw = kernel.shape
-    convolution = np.zeros((m, h - kh, w - kw))
+    convolution = np.zeros((m, h - kh + 1, w - kw + 1))
     imgs = np.arange(m)
-    for h2 in range(h - kh):
-        for w2 in range(w - kw):
+    for h2 in range(h - kh + 1):
+        for w2 in range(w - kw + 1):
             img_slct = images[:, h2:h2 + kh, w2:w2 + kw]
             convolution[imgs, h2, w2] = np.sum(img_slct * kernel, axis=(1, 2))
     return convolution
