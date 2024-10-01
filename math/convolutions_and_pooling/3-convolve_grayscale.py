b6e7baff6d4ed0e299876e5a37d32b247ebd3fb3
FrancoMuVa <muvafranco@gmail.com>
2024-10-01 15:26:27 +0000

    fix: dimentions of output

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -17,9 +17,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     else:
         ph, pw = padding
     pad_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
-    _, h_pad, w_pad = pad_img.shape
-    h_output = (h_pad - kh + ph * 2) // sh + 1
-    w_output = (w_pad - kw + pw * 2) // sw + 1
+    h_output = (h - kh + ph * 2) // sh + 1
+    w_output = (w - kw + pw * 2) // sw + 1
     convolution = np.zeros((m, h_output, w_output))
     imgs = np.arange(m)
     for h2 in range(h_output):
